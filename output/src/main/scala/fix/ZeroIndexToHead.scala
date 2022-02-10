aa9d175b4e2c5d542d935532e2f6a1a1d6efb6ef
Javakky-pxv <javakky@pixiv.co.jp>
2022-02-11 00:13:06 +0900

    `(0)`` インデックス呼び出しを `.head` に置き換え

---
+++ output/src/main/scala/fix/ZeroIndexToHead.scala
@@ -0,0 +1,14 @@
+package fix
+
+object ZeroIndexToHead {
+  val seq = Seq(1, 2, 3)
+  seq.head
+
+  Seq(1, 2, 3).head
+
+  val list = List(1, 2, 3)
+  list.head
+
+  val indexed = IndexedSeq(1, 2, 3)
+  indexed(0)
+}
