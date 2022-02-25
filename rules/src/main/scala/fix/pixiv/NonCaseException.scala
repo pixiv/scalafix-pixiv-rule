b43ff48d7dcdae7081fec25d0f265b34e9b506a8
Javakky-pxv <javakky@pixiv.co.jp>
2022-02-25 15:12:18 +0900

    case な Exception に警告を出す

---
+++ rules/src/main/scala/fix/pixiv/NonCaseException.scala
@@ -0,0 +1,23 @@
+package fix.pixiv
+
+import scala.meta.{Defn, Init, Mod, Position, Template}
+
+import scalafix.Patch
+import scalafix.lint.{Diagnostic, LintSeverity}
+import scalafix.v1.{SemanticDocument, SemanticRule, XtensionTreeScalafix}
+import util.SymbolConverter.SymbolToSemanticType
+
+class NonCaseException extends SemanticRule("NonCaseException") {
+
+  override def fix(implicit doc: SemanticDocument): Patch = {
+    doc.tree.collect {
+      case t @ Defn.Class(List(_: Mod.Case), name, _, _, _ @Template(_, List(_ @Init(typ, _, _)), _, _))
+          if typ.symbol.isAssignableTo(classOf[Exception]) =>
+        Patch.lint(NonCaseExceptionWarn(s"case class として Exception を継承することは推奨されません: $name", t.pos))
+    }.asPatch
+  }
+}
+
+case class NonCaseExceptionWarn(override val message: String, position: Position) extends Diagnostic {
+  override def severity: LintSeverity = LintSeverity.Warning
+}
