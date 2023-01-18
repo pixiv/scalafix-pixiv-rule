2903a631029e7cb43a15c8201e66fbbfd71e9de3
takenoko <takenoko@pixiv.co.jp>
2023-01-18 13:44:33 +0900

    input,outputのテストケース修正/追加

--- input/src/main/scala-2.13/fix/pixiv/ScalatestAssertThrowsToIntercept.scala
+++ input/src/main/scala-2.13/fix/pixiv/ScalatestAssertThrowsToIntercept.scala
@@ -4,10 +4,14 @@ rule = ScalatestAssertThrowsToIntercept
 package fix.pixiv
 
 import org.scalatest.Assertions
+import org.scalatest.Assertions._
 
 object ScalatestAssertThrowsToIntercept {
-  Assertions.intercept[RuntimeException] {
-    throw new RuntimeException("assertThrows")
+  Assertions.assertThrows[RuntimeException] {
+    throw new RuntimeException("assertThrows1")
+  }
+  assertThrows[RuntimeException]{
+    throw new RuntimeException("assertThrows2")
   }
   //変更なし
   println("assertThrows")
--- output/src/main/scala-2.13/fix/pixiv/ScalatestAssertThrowsToIntercept.scala
+++ output/src/main/scala-2.13/fix/pixiv/ScalatestAssertThrowsToIntercept.scala
@@ -1,10 +1,14 @@
 package fix.pixiv
 
 import org.scalatest.Assertions
+import org.scalatest.Assertions._
 
 object ScalatestAssertThrowsToIntercept {
   Assertions.intercept[RuntimeException] {
-    throw new RuntimeException("assertThrows")
+    throw new RuntimeException("assertThrows1")
+  }
+  intercept[RuntimeException]{
+    throw new RuntimeException("assertThrows2")
   }
   //変更なし
   println("assertThrows")
