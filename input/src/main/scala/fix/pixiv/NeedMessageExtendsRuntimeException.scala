4a2ca6df8c6ea4326f2bfc829f0028fa24f3c78a
kobakazu0429 <kobakazu10@gmail.com>
2023-03-23 18:02:46 +0900

    sbt scalafmtSbt "src/scalafmtAll" "input2_13/scalafmtAll" "input2_12/scalafmtAll"

--- input/src/main/scala/fix/pixiv/NeedMessageExtendsRuntimeException.scala
+++ input/src/main/scala/fix/pixiv/NeedMessageExtendsRuntimeException.scala
@@ -5,7 +5,7 @@ package fix.pixiv
 
 class GreenException extends RuntimeException("message") {}
 
-class RedException extends RuntimeException {}/* assert: NeedMessageExtendsRuntimeException
+class RedException extends RuntimeException {} /* assert: NeedMessageExtendsRuntimeException
 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 RuntimeException を継承したクラスを作る際にはメッセージを付与してください: RedException
  */
